2a48eb5d461388e6de7dd34d808f956992ed29fd
Deny Rianto <denyryn@outlook.com>
2024-12-07 19:09:36 +0700

    feat: add topics repository to centralize topic definitions

---
+++ lib/repositories/topics_repository.dart
@@ -0,0 +1,5 @@
+class TopicsRepository {
+  static const String ledsTopic = 'denyryn/datas/leds';
+  static const String dhtTopic = 'denyryn/datas/dht';
+  static const String ldrTopic = 'denyryn/datas/ldr';
+}
