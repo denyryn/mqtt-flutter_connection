044a398eef73f219073c48571803fd65950a942e
Deny Rianto <denyryn@outlook.com>
2024-12-09 00:04:27 +0700

    feat(repository): add LED configuration to initialization

--- lib/repositories/leds_initialization.dart
+++ lib/repositories/leds_initialization.dart
@@ -3,8 +3,10 @@ import '../models/led_model.dart';
 
 class LedsInitialization {
   final RxList<LedModel> ledList = <LedModel>[
-    LedModel(pin: 'D0', status: false, brightness: 0),
+    LedModel(pin: 'D1', status: false, brightness: 0),
+    LedModel(pin: 'D2', status: false, brightness: 0),
+    LedModel(pin: 'D3', status: false, brightness: 0),
     LedModel(pin: 'D4', status: false, brightness: 0),
-    LedModel(pin: 'D6', status: false, brightness: 0),
+    LedModel(pin: 'D5', status: false, brightness: 0),
   ].obs;
 }
